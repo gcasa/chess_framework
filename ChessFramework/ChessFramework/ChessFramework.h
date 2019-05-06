7fc1a67382703bd8f7405d01eebfa71feef51979
Gregory John Casamento <greg.casamento@gmail.com>
2019-05-06 16:01:29 -0400

    Initial commit

---
+++ ChessFramework/ChessFramework/ChessFramework.h
@@ -0,0 +1,19 @@
+//
+//  ChessFramework.h
+//  ChessFramework
+//
+//  Created by Gregory John Casamento on 5/6/19.
+//  Copyright © 2019 Gregory John Casamento. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ChessFramework.
+FOUNDATION_EXPORT double ChessFrameworkVersionNumber;
+
+//! Project version string for ChessFramework.
+FOUNDATION_EXPORT const unsigned char ChessFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChessFramework/PublicHeader.h>
+
+
